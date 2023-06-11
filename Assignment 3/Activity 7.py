46cfc4852d6ee1ec48f6a333013ccd06e0d32556
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-11 18:36:06 -0500

    Update Activity 7.py
    adding references

--- Assignment 3/Activity 7.py
+++ Assignment 3/Activity 7.py
@@ -1,4 +1,5 @@
 # This program displays your dogs age in puppy years when given human years
+# References: https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-2/
 print("What is your Dog's name?")
 dogsName = input()
 print("How old is your dog, in human years?")
