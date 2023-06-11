e2e1791c481ffb01c20bfc6c4432e6a90480fa62
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-11 18:33:46 -0500

    Delete Activity 7.py

--- Assignment 3/Activity 7.py
+++
@@ -1,6 +0,0 @@
-# This program displays your dogs age in puppy years when given human years
-print("What is your Dog's name?")
-dogsName = input()
-print("How old is your dog, in human years?")
-dogsAge = int(input())
-print(dogsName + " is " + str(dogsAge * 7) + " years young, in human years! ")
