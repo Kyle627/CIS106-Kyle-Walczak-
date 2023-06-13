793c100ceb648aa5886e9342b08c29c720945dd5
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-13 01:47:36 -0500

    line size change

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -7,7 +7,8 @@
 print("What is your name?")
 name_one = str(input())
 
-print("What is your age in years? (X.25 = X and 3 months old, .50 = X and 6 months old, .75 = x and 9 months old)")
+print("What is your age in years? (X.25 = X and 3 months old, "
+      ".50 = X and 6 months old, .75 = x and 9 months old)")
 age_years = float(input()) 
 age_months = int(age_years * 12)
 age_days = int(age_years * 365.25)
@@ -15,6 +16,7 @@ age_hours = int(age_days * 24)
 age_min = int(age_hours * 60)
 age_sec = int(age_min * 60 ) 
 
-print(str(name_one) + ", being " + str(age_years) + " years old you are: " + str(age_months) + " months old, " + str(age_days) + " days old, " + str(age_min) + " Minutes old, and " + str(age_sec) + " seconds old. PHEW!")
+print(str(name_one) + ", being " + str(age_years) + " years old you are: " + str(age_months) + " months old, "
+      + str(age_days) + " days old, " + str(age_min) + " Minutes old, and " + str(age_sec) + " seconds old. PHEW!")
 
 
