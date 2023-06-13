419b8e4a3ab84a082145482ae2017ffc2ee47ba0
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-13 01:24:32 -0500

    Add files via upload

---
+++ Assignment 4/Activity 2.py
@@ -0,0 +1,20 @@
+# This program converts inputted age into months, days, hours, minutes, and seconds.
+#
+# References:
+# https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-1/
+# https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-2/
+
+print("What is your name?")
+Name = str(input())
+
+print("What is your age in years? (X.25 = X and 3 months old, .50 = X and 6 months old, .75 = x and 9 months old)")
+AgeYears = float(input()) 
+AgeMonths = int(AgeYears * 12)
+AgeDays = int(AgeYears * 365.25)
+AgeHours = int(AgeDays * 24)
+AgeMin = int(AgeHours * 60)
+AgeSec = int(AgeMin * 60 ) 
+
+print(str(Name) + ", being " + str(AgeYears) + " years old you are: " + str(AgeMonths) + " months old, " + str(AgeDays) + " days old, " + str(AgeMin) + " Minutes old, and " + str(AgeSec) + " seconds old. PHEW!")
+
+
