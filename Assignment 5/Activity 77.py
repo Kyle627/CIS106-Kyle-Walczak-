658dd96c7f8a36801e85e827069a6be4f1b40042
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-21 00:06:21 -0500

    Add files via upload

---
+++ Assignment 5/Activity 77.py
@@ -0,0 +1,28 @@
+def displayResult():
+    print(dogsName + " being " + str(dogsAge) + " is " + str(dogsRealAge) + "in human years.")
+    
+    return 
+
+def getAge():
+    print("What is your dogs age?")
+    dogsAge = int(input())
+    
+    return dogsAge
+
+def getName():
+    print("What is your dogs name?")
+    dogsName = input()
+    
+    return dogsName
+
+def getRealAge():
+    realAge = dogsAge * 7
+    
+    return realAge
+
+# Main
+# This program takes inputted dog age and name and outputs the converted age with a prompt.
+dogsName = getName()
+dogsAge = getAge()
+dogsRealAge = getRealAge()
+DisplayResults()
