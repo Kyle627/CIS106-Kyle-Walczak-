71c069ae8e6e343eca0acf3d518a6acf2ebaa428
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-22 15:18:42 -0500

    Update Activity 2.py

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -7,9 +7,6 @@
 # https://harpercollege.pressbooks.pub/programmingfundamentals/
 # chapter/python-examples-2/
 
-print("What is your name?")
-name_one = str(input())
-
 print("What is your age in years? (X.25 = X and 3 months old, "
       ".50 = X and 6 months old, .75 = x and 9 months old)")
 age_years = float(input()) 
@@ -19,5 +16,5 @@ age_hours = int(age_days * 24)
 age_min = int(age_hours * 60)
 age_sec = int(age_min * 60) 
 
-print(str(name_one) + ", being " + str(age_years) + " years old you are: " + str(age_months) + " months old, " +
+print(str(age_years) + " years old you are: " + str(age_months) + " months old, " +
        str(age_days) + " days old, " + str(age_hours) + " hours old, " + str(age_min) + " Min old, and " + str(age_sec) + " seconds old. PHEW!")
