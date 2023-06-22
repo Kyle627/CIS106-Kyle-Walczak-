5416ac34b31aa2922ced6abdd3fc309fb3377a0f
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-22 15:20:50 -0500

    Update Activity 2.py

--- Assignment 4/Activity 2.py
+++ Assignment 4/Activity 2.py
@@ -9,12 +9,19 @@
 
 print("What is your age in years? (X.25 = X and 3 months old, "
       ".50 = X and 6 months old, .75 = x and 9 months old)")
+
 age_years = float(input()) 
+
 age_months = int(age_years * 12)
+
 age_days = int(age_years * 365.25)
+
 age_hours = int(age_days * 24)
+
 age_min = int(age_hours * 60)
+
 age_sec = int(age_min * 60) 
 
-print(str(age_years) + " years old you are: " + str(age_months) + " months old, " +
-       str(age_days) + " days old, " + str(age_hours) + " hours old, " + str(age_min) + " Min old, and " + str(age_sec) + " seconds old. PHEW!")
+print(str(age_years) + " years old you are: " + str(age_months) +
+      " months old, " + str(age_days) + " days old, " + str(age_hours) + 
+      " hours old, " + str(age_min) + " Min old, and " + str(age_sec) + " seconds old. PHEW!")
