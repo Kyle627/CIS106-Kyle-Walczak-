1136ead6e97d7b2942c90d0497fc4c9d7ae2801c
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-06-27 20:48:51 -0500

    Update Activity 1.py

--- Assignment 8/Activity 1.py
+++ Assignment 8/Activity 1.py
@@ -1,1 +1,39 @@
-##
+# This program takes iteration input from user and outputs 
+# a multiplication table with x(input) number of expressions 
+#
+# Referecnce: 
+# https://harpercollege.pressbooks.pub/programmingfundamentals
+# /chapter/python-examples-5/
+# https://www.w3schools.com/python/python_for_loops.asp
+
+
+def get_value():
+    print("What value would you like to multiply by?")
+    value = int(input())
+    return value
+
+
+def get_iteration():
+    print("How many expressions would like to run?")
+    iterations = int(input())
+    return iterations
+
+
+def demonstrate_for_loop(iterations, value):
+    for x in range(1, iterations +1):
+        display_result(value, x)
+        x += 1
+
+
+def display_result(value, x):
+    print(f"{value} x {x} = {x*value}")
+    return
+
+
+def main():
+    value = get_value()
+    iterations = get_iteration()
+    demonstrate_for_loop(iterations, value) 
+
+
+main()
