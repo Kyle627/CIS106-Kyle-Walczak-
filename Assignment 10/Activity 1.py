b6812c5a9795a7c2b80c5f5904d83a1cefce7d09
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-07-05 16:56:01 -0500

    Update Activity 1.py

--- Assignment 10/Activity 1.py
+++ Assignment 10/Activity 1.py
@@ -1,1 +1,39 @@
+# This program takes test scores (amount of tests is dependent on user 
+# input) and averages the scores. 
+#
+# References:
+# https://harpercollege.pressbooks.pub/programmingfundamentals
+# /chapter/python-examples-5/
+# https://www.w3schools.com/
+# python/python_while_loops.asp
 
+
+def get_score(): 
+    total = 0 
+    counter = 0
+    while True: 
+        print("Enter test score:")
+        score = int(input())
+        if score < 0:
+            break
+        total += score
+        counter += 1
+    return total, counter
+ 
+def calculate_average(total, counter):
+    average_score = total / counter
+    return average_score
+
+
+def display_results(average_score):
+    print(f"Average Score: {average_score}")
+    return
+
+
+def main():
+    total, counter = get_score()
+    average_score = calculate_average(total, counter)
+    display_results(average_score)
+
+
+main()
