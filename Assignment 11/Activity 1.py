23acc2bd5bec45350a21b4c30e81be8d5e9e63bc
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-07-09 15:57:29 -0500

    Update Activity 1.py

--- Assignment 11/Activity 1.py
+++ Assignment 11/Activity 1.py
@@ -22,11 +22,10 @@ def array_process(year, month):
     months = ['January', 'February', 'March', 'April',
               'May', 'June', 'July', 'September',
               'October', 'November', 'December']
-    days = [31,28,31,30,31,30,31,31,30,31,30,31]
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    if month  == 'February' and year % 4 == 0 and (year % 100 != 0
-            or year % 400 == 0):
-            days[1] = 29
+    if month == 'February' and year % 4 == 0 and (year % 100 != 0
+            or year % 400 == 0): days[1] = 29
     return days[months.index(month)]
 
 
