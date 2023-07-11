7f84599b6f277e3f7cac7fcbff876a84a0fa6bdd
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-07-10 21:00:38 -0500

    Update Activity 1.py

--- Assignment 11/Activity 1.py
+++ Assignment 11/Activity 1.py
@@ -5,11 +5,12 @@
 # https://harpercollege.pressbooks.pub/
 # programmingfundamentals/chapter/python-examples-6/
 # https://www.w3schools.com/python/python_lists_access.asp
+# https://www.w3schools.com/python/ref_list_insert.asp
 
 
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 name_month = ['January', 'February', 'March', 'April',
-              'May', 'June', 'July', 'September', 
+              'May', 'June', 'July', 'August', 'September', 
               'October', 'November', 'December']
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
@@ -23,21 +24,21 @@ def get_year():
 def get_month():
     print("What month would you like to know about?")
     month = int(input()) 
+    if month < 1 or month > 12:
+        print("Invalid month. Month must be between 0-12")
+        quit()
     return month
 
 
 def array_process(year, month):    
     if month == 2 and year % 4 == 0 and (year % 100 != 0
-            or year % 400 == 0):
-            days[1] = 29
+            or year % 400 == 0): 
+                days.insert(1, 29)
     return days[months.index(month)]
 
 
 def display_result(year, month, days):
-    if month == 12:
-        print(f" December {year} has {days} days")
-    if month != 12:
-         print(f" {name_month[month-1]} {year} has {days} days")
+    print(f"{name_month[month-1]} {year} has {days} days")
     return 
 
 
