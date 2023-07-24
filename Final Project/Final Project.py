6a6d53c3194bf6e0c522f3affad7c47b3a038691
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-07-23 21:14:41 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -4,3 +4,19 @@
 # References:
 # https://harpercollege.pressbooks.pub/programmingfundamentals
 # /chapter/python-examples-8/
+def read_menu(menu):
+    try:
+        with open(menu, "r") as file:
+            for line in file:
+                    content = line.strip()
+    except Exception:
+        print("File not found")
+    return content
+    
+def main():
+    menu = "menu.xml"
+    content = read_menu(menu)
+    
+    
+    
+main()
