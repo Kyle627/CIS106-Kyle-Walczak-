b3a47ab76bf6407312321e5621a5ac4fdf24d09b
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-07-24 16:31:20 -0500

    Update Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -58,4 +58,42 @@ def main():
     
 main()
 
+______________________________________________________________________________________
+NOT SURE IF I WANT TO DELETE THIS^^^ YET.
+# This program reads menu provided and outputs all items respectively.
+# Then lists the total items, average calories, and average price.
+#
+# References:
+# https://harpercollege.pressbooks.pub/programmingfundamentals
+# /chapter/python-examples-8/
+# https://youtu.be/K8L6KVGG-7o
+# https://docs.python.org/3/library/stdtypes.html#str.isdigit
+# https://stackoverflow.com/questions/22247957/regex-to-find-words-between-two-tags
+import re
 
+def read_menu(menu):
+    try:
+        with open(menu, "r") as file:
+            for line in file:
+                    content = file.read()
+    except Exception:
+        print("File not found")
+    return content
+    
+def get_item(content):
+    names = []
+    names = re.findall("<name>(.*?)</name>", content, re.DOTALL)
+    print(f"{names}")
+    print(f"{content}")
+    print(type(names))
+    
+
+
+def main():
+    menu = "menu.xml"
+    content = read_menu(menu)
+    names = get_item(content)
+    
+    
+    
+main()
