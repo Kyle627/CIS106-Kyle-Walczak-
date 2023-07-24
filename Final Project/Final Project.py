234f6a27509c8c67c22d56d3606f5f6707cd9b82
Kyle627 <135172467+Kyle627@users.noreply.github.com>
2023-07-23 20:21:54 -0500

    Create Final Project.py

---
+++ Final Project/Final Project.py
@@ -0,0 +1,6 @@
+# This program reads menu provided and outputs all items respectively.
+# Then lists the total items, average calories, and average price.
+#
+# References:
+# https://harpercollege.pressbooks.pub/programmingfundamentals
+# /chapter/python-examples-8/
